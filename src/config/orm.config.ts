e503e39a4097210786a42e8dbcf3178e22e26c9e
ndharu <96077141+ndharu@users.noreply.github.com>
2022-08-21 17:44:27 +0700

    delete some value on .env file

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 import {
   TypeOrmModuleAsyncOptions,
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-export const config: TypeOrmModuleOptions = {
+export const config: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
   useFactory: async (): Promise<TypeOrmModuleOptions> => {
     return {
-      // type: 'postgres',
-      // host: 'localhost',
-      // port: 5432,
-      // username: 'postgres',
-      // password: 'asdqwe123',
-      // database: 'pttodis',
-      // entities: ['dist/**/*.entity.js'],
-      // synchronize: true,
-      // logging: true,
-      // migrations: ['dist/**/*.entity.js'],
-      // migrationsTableName: 'migration',
-
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT, 10),
@@ -59,20 +46,4 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   },
   synchronize: false,
   logging: true,
-=======
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const config: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'asdqwe123',
-  database: 'pttodis',
-  entities: ['dist/**/*.entity.js'],
-  synchronize: true,
-  logging: true,
-  migrations: ['dist/**/*.entity.js'],
-  migrationsTableName: 'migration',
->>>>>>> 111eed0cb652437d3e3905f85e8c26fcc94e1ae5
 };
